6ed3fe7dfa4417f0ac7e90097c186f89e33be686
chrptos <chrptos@gmail.com>
2023-05-19 19:55:21 +0900

    init

---
+++ pages/products/_id/edit.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        edit<br>
+        {{ $route.params }}
+        {{ $route.params.id }}
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/products/_id/show.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        show
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
